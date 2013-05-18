2fbcdd2205a10acb949b618003f663b6baf5659b
Wes Gamble <weyus@att.net>
2013-05-18 10:30:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slimeintheicemachine::Application.config.secret_key_base = '1dca8e04c0bf17e9357a91583feb52d4f74a41a00eff062e41a289cc49f9ec31c4bf9d664f7f166aad38ef96bf82cac8214edd0685dd254833d564d523b593c3'
